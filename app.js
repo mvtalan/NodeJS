3aef1ee7f762e3335d74512a7e59c7ea1041f569
mvtalan <mvtalan@gmail.com>
2024-08-01 11:40:34 -0600

    with ejs file

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 const PORT = 3000;
+
+//setting the view engine
+app.set('view engine', 'ejs');
+app.set('veiws', './views');
 
 //middleware
 // app.use((req,res, next) => {
@@ -9,13 +14,19 @@ const PORT = 3000;
 //     next();
 // })
 
+// route controller with a path parameter
 app.get('/users/:id', (req, res) => {
     const userId = req.params.id;
     res.send(`User with ID ${userId}`);
 })
 
 app.get('/', (req, res) => {
-    res.send('Hello Red Deer!');
+    
+    const data = {
+        title: 'Hello Red Deer!',
+        message: 'Welcome to my website.'
+    }
+    res.render('index', {data});
 });
 
 app.listen(3000, () => {
