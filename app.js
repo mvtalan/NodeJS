4134d6bee622ae1a49e98b9be308ba5f7ef52c9c
mvtalan <mvtalan@gmail.com>
2024-08-01 10:47:02 -0600

    with new routes

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+//middleware
+// app.use((req,res, next) => {
+//     console.log('Middleware Executed.');
+//     next();
+// })
+
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(`User with ID ${userId}`);
+})
+
 app.get('/', (req, res) => {
-    res.send('Hello!');
+    res.send('Hello Red Deer!');
 });
 
 app.listen(3000, () => {
